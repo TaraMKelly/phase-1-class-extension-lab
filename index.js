86adb91d38296802445eb32039455ba432160520
TaraMKelly <tara.kelly16@gmail.com>
2021-07-15 22:07:54 -0400

    commit tree d508a3b0478883494ddabc04a425d2f4348b399c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Polygon {
+    constructor(integerArray) {
+        this.integerArray = integerArray
+    }
+    get countSides() {
+        return this.integerArray.length
+    }
+    get perimeter() {
+        return this.integerArray.reduce((a, b) => a + b, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if(this.integerArray[0]===this.integerArray[1]||this.integerArray[0]===this.integerArray[2]||this.integerArray[1]===this.integerArray[2]){
+            return true
+        }
+        else return false
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if((this.perimeter/4)===this.integerArray[0]) return true
+        else return false
+    }
+    get area() {
+        return this.integerArray[0] ** 2
+    }
+}
